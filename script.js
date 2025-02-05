fe95704d0aeb4e52d3d60722ba14fe8a8497aee5
matteo-giustini <matteo.giustini3@gmail.com>
2025-02-05 12:29:26 +0100

    feat: end of project

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const lampOnElm = document.getElementById("lampOn")
 const lampOffElm = document.getElementById("lampOff")
 const btnElm = document.getElementById("btn")
 
-btnElm.addEventListener("click",
-    function () {
-        lampOnElm.style.display = "block"
-        lampOffElm.style.display = "none"
-        console.log("tasto cliccato")
+
+
+btnElm.addEventListener("click", function () {
+       if(lampOnElm.classList.contains("hidden")){
+        lampOnElm.classList.remove("hidden")
+        lampOffElm.classList.add("hidden")
+        btnElm.innerText = "Spegni"
+       } else{
+        lampOnElm.classList.add("hidden")
+        lampOffElm.classList.remove("hidden")
+        btnElm.innerText = "Accendi"
+       } 
+       
     }
 
 )
